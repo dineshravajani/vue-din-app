2aafa1afd6675c4c54e3ee6097fddf821abe1260
Din Turner <dinturner01@gmail.com>
2024-12-20 12:19:47 +0530

    first commit

---
+++ src/components/Article.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Article Component</h1>
+        <h1>{{ title }}</h1>
+        <h1>Likes : {{ likes }}</h1>
+        <h1>Published : {{  isPublished ? 'Yes' : 'No' }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Article",
+        props:{
+            title:String,
+            likes:Number,
+            isPublished:Boolean,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Hello {{ name  }} {{ lastname }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Greet",
+        props:["name","lastname"]
+    }
+</script>
+
+<style scoped>
+
+</style>
